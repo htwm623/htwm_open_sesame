5884c608241ca97706fa873bb52f61ab4a882731
htwm623 <htwm623@outlook.com>
2020-08-21 00:59:38 +0800

    完成02_exercise

--- 30DaysOfJavaScript/02_exercises.js
+++ 30DaysOfJavaScript/02_exercises.js
@@ -74,3 +74,47 @@ console.log('30 Days of'.concat('JavaScript'))
 
 // Use repeat() method to print 30 Days Of JavaScript 2 times
 console.log('30 Days Of JavaScript'.repeat(2))
+
+
+
+
+// Exercise: Level 2
+ 
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
+
+console.log("\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\"")
+
+console.log(typeof '10' === typeof 10)
+
+// Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+console.log(parseFloat('9.8') === 10)
+
+// Check if 'on' is found in both python and jargon
+console.log('python'.search('on') > 0 && 'jargon'.search('on') > 0)
+
+// I hope this course is not full of jargon. Check if jargon is in the sentence.
+console.log('I hope this course is not full of jargon'.search('jargon') !== -1)
+
+// Generate a random number between 0 and 100 inclusively.
+console.log(parseInt(Math.random() * 100 ))
+
+// Generate a random number between 50 and 100 inclusively.
+console.log(parseInt(50 + Math.random() * 50))
+
+// Generate a random number between 0 and 255 inclusively.
+console.log(parseInt(Math.random() * 255))
+
+// Access the 'JavaScript' string characters using a random number.
+console.log('JavaScript'[parseInt(Math.random() * 'JavaScript'.length)])
+
+
+// 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+console.log('Love is the best thing in this world. Some found their love and some are still looking for their love.'.match(/love/gi).length)
+
+// Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log('You cannot end a sentence with because because because is a conjunction'.match(/because/gi).length)
+
+// Clean the following text(hint, use replace and regular expressions).
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+console.log(sentence.replace(/%|@|\$|\?|#|&|\;/gi, ''))
+
